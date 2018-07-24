51a44fd5605d4eb28fc3b857634a4f0e62856683
Vishal Sharma <vsh1819@gmail.com>
2018-07-24 13:22:41 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,30 +17,38 @@ class SongsController < ApplicationController
 
   def show
   
-  
+   if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+      if @song.nil?
+        redirect_to artist_songs_path(@artist), alert: "Song not found"
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   
       
     
-    @song = Song.find_by(params[:id])
-    # @artist = @song.artist
+    # @song = Song.find_by(params[:id])
+    # # @artist = @song.artist
     
     
   
-    # if params[:artist_id]
-        # @song = Song.find_by(id: params[:id])
-        # @artist = @song.artist
+    # # if params[:artist_id]
+    #     # @song = Song.find_by(id: params[:id])
+    #     # @artist = @song.artist
     
-              # @artist = Artist.find_by(id: params[:artist_id])
+    #           # @artist = Artist.find_by(id: params[:artist_id])
 
-        if Song.find_by(id: params[:id]).nil?
+    #     if Song.find_by(id: params[:id]).nil?
           
-          flash[:alert] = "Song not found."
-           redirect_to artist_songs_path
-        else
-          @song = Song.find_by(params[:id])
-        end 
-      # else
-      #             @song = Song.find_by(params[:id])
+    #       flash[:alert] = "Song not found."
+    #       redirect_to artist_songs_path
+    #     else
+    #       @song = Song.find_by(params[:id])
+    #     end 
+    #   # else
+    #   #             @song = Song.find_by(params[:id])
 
     # end
     
