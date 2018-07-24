03b66d2e9390745e5a0851669a02a94f3adc3424
Vishal Sharma <vsh1819@gmail.com>
2018-07-24 12:46:54 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,7 +27,7 @@ class SongsController < ApplicationController
     
   
     # if params[:artist_id]
-        @song = Song.find_by(id: params[:id])
+        # @song = Song.find_by(id: params[:id])
         # @artist = @song.artist
     
               # @artist = Artist.find_by(id: params[:artist_id])
